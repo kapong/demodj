380b80d2510a0af783f5c10b0a82904bd1eb55c1
kapong <garpong@gmail.com>
2016-03-23 04:36:23 +0700

    remove sass

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ var express = require('express');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
-var sass = require('node-sass-middleware');
+// var sass = require('node-sass-middleware');
 
 module.exports = function(){
 	var app = express();
@@ -22,14 +22,14 @@ module.exports = function(){
 	app.set('view engine','jade');
 
 	require('../app/routes/index.routes')(app);
-	app.use(sass({
-		src: './sass',
-		dest: './public/css',
-		outputStyle: 'compressed',
-		prefix: '/css',
-		debug: true,
-		indentedSyntax: true
-	}))
+	// app.use(sass({
+	// 	src: './sass',
+	// 	dest: './public/css',
+	// 	outputStyle: 'compressed',
+	// 	prefix: '/css',
+	// 	debug: true,
+	// 	indentedSyntax: true
+	// }))
 
 	app.use(express.static('./public'));
 	return app;
